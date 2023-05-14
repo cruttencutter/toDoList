ecab52b86f833faef469304cf0b7c993047b628b
Claire Ruttencutter <51419244+cruttencutter@users.noreply.github.com>
2023-05-14 14:45:35 -0500

    Initial Commit

---
+++ toDoList/toDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoListApp.swift
+//  toDoList
+//
+//  Created by Claire Ruttencutter on 5/14/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
